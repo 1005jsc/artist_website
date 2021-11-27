a2bc31c700ffb86b16dcf2a98a43e1195dc0de97
1005jsc <1005jsc@naver.com>
2021-11-28 05:48:03 +0900

    artist_bio scheme finished

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BiographyBio from '../biography_bio/biography_bio';
 import BiographyPicture from '../biography_picture/biography_picture';
 import BiographyWords from '../biography_words/biography_words';
 import styles from './section.module.css'
@@ -7,9 +8,13 @@ import styles from './section.module.css'
 const Section = () => {
 
   return <section className={styles.section}>
-    <div className={styles.bio_words_container}>
+    {/* <div className={styles.bio_words_container}>
     <BiographyWords/>
+    </div> */}
+    <div className={styles.bio_words_container}>
+    <BiographyBio/>
     </div>
+
     <div className={styles.bio_picture_container}>
     <BiographyPicture/>
     </div>
