bca98da611eab8db77e0dd2bc63f9ea9c69401d0
1005jsc <1005jsc@naver.com>
2021-11-27 19:19:30 +0900

    navbar_left_basic_design_done(logo_design_needed)

--- src/components/navbar_left/navbar_left.tsx
+++ src/components/navbar_left/navbar_left.tsx
@@ -5,8 +5,48 @@ import styles from './navbar_left.module.css'
 const NabvarLeft = () => {
 
 return <nav className={styles.navbar_left}>
-  <h1 className={styles.h1}>Navbar left</h1>
-  <h1>Navbar left</h1>
+  
+  <div className={styles.logo_sample}>Logo</div>
+
+  <div className={styles.navbar_container}>
+
+    <span className={`${styles.biography_left} ${styles.span_first} ${styles.biography_left_onclick}`}>작가소개</span>
+    <div className={`${styles.biography_container} ${styles.biography_left_onclick_invisible_on}`}>
+      <span className={`${styles.artist_note} ${styles.h2}`}>- 작가노트</span>
+      <span className={`${styles.bio} ${styles.h2}`}>- 작가연혁</span>
+    </div>
+    
+    <span className={`${styles.works_left} ${styles.span_first} ${styles.works_left_onclick}`}>작품</span>
+      <div className={`${styles.works_container} ${styles.works_left_onclick_invisible_on}`}>
+      
+        <div className={`${styles.work} ${styles.work_국윤미술관_기획초대전}`}>
+          <span className={styles.h2}>- 국윤미술관 기획초대전</span>
+          <div className={styles.work_국윤미술관_기획초대전_onclick}>
+            <span className={styles.h3}>- 시간을 담다</span>
+            <span className={styles.h3}>- 빛을 담다</span>
+            <span className={styles.h3}>- 길을 걷다</span>
+          </div>
+        </div>
+      <div className={`${styles.work} ${styles.work_국윤미술관_기획초대전}`}>
+        <span className={styles.h2}>- 국윤미술관 기획초대전</span>
+        <div className={styles.work_국윤미술관_기획초대전_onclick}>
+          <span className={styles.h3}>- 시간을 담다</span>
+          <span className={styles.h3}>- 빛을 담다</span>
+          <span className={styles.h3}>- 길을 걷다</span>
+        </div>
+      </div>
+
+      </div>
+    
+    <span className={`${styles.critics_left} ${styles.span_first} ${styles.critics_left_onclick}`}>평론가의 글</span>
+      <div className={`${styles.critics_container} ${styles.critics_left_onclick_invisible_on}`}>
+        <span className={styles.h2}>- 시간속에 담아낸 내면의 추상언어 &lt;조인호&gt;</span>
+        <span className={styles.h2}>- 시간속에 담아낸 내면의 추상언어 &lt;조인호&gt;</span>
+      </div>
+    <span className={`${styles.contacts_left} ${styles.span_first}`}>연락처</span>
+
+  </div>
+  
 
 </nav>
 
