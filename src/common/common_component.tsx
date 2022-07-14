235c133560a0c32b9921ea4322b70464221db960
cho_jae_sin <1005jsc@naver.com>
2022-07-14 22:03:44 +0900

    1

--- src/common/common_component.tsx
+++ src/common/common_component.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-import styles from "./common_component.module.css";
+import React from 'react';
+import styles from './common_component.module.css';
 
 const CommonComponent = () => {
-
-  return <article className={styles.container}>
-    <div className={styles.container2}>
-    <div className={styles.title_container}>
-  <span className={styles.title}>작가노트</span>
-  </div>
-    여기에 내용물이 들어감 
-    </div>
-</article> 
-
-
-
-}
+  return (
+    <article className={styles.container}>
+      <div className={styles.container2}>
+        <div className={styles.title_container}>
+          <span className={styles.title}>작가노트</span>
+        </div>
+        여기에 내용물이 들어감
+      </div>
+    </article>
+  );
+};
 export default CommonComponent;
--- src/components/main/medium/work_folder/work_modal/work_modal.tsx
+++ src/components/main/medium/work_folder/work_modal/work_modal.tsx
@@ -6,147 +6,143 @@ import styled from 'styled-components';
 import { PopupInModal } from '../popup_in_modal/popup_in_modal';
 
 type WorkModalProps = {
-	modalOff: () => void;
-	workUrl: string | null | undefined;
+  modalOff: () => void;
+  workUrl: string | null | undefined;
 };
 
 type FrameProps = {
-	ratio: number;
-	mouseStartX: number;
-	mouseStartY: number;
+  ratio: number;
+  mouseStartX: number;
+  mouseStartY: number;
 };
 
 const WorkModal = ({ modalOff, workUrl }: WorkModalProps) => {
-	// 줌앤 패닝  줌 비율
-	const [ratio, setRatio] = useState(0.5);
+  // 줌앤 패닝  줌 비율
+  const [ratio, setRatio] = useState(0.5);
 
-	const frameRef = useRef<HTMLDivElement>(null);
-	const imageContainerRef = useRef<HTMLDivElement>(null);
+  const frameRef = useRef<HTMLDivElement>(null);
+  const imageContainerRef = useRef<HTMLDivElement>(null);
 
-	const handleModalOff: React.MouseEventHandler<HTMLDivElement> = (e) => {
-		e.preventDefault();
+  const handleModalOff: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
 
-		if (e.currentTarget === e.target) {
-			modalOff();
-		}
-	};
+    if (e.currentTarget === e.target) {
+      modalOff();
+    }
+  };
 
-	const handleKeyDetector = (keyValue: string) => {
-		if (keyValue === 'Escape') {
-			modalOff();
-		}
-	};
+  const handleKeyDetector = (keyValue: string) => {
+    if (keyValue === 'Escape') {
+      modalOff();
+    }
+  };
 
-	// 스크롤의 기준점
+  // 스크롤의 기준점
 
-	const [mouseStartX, setMouseStartX] = useState<number>(0);
-	const [mouseStartY, setMouseStartY] = useState<number>(0);
+  const [mouseStartX, setMouseStartX] = useState<number>(0);
+  const [mouseStartY, setMouseStartY] = useState<number>(0);
 
-	const wheelHandler: React.WheelEventHandler<HTMLDivElement> = (e) => {
-		let imageContainer;
-		let offsetX = 0;
-		
-		if (imageContainerRef.current) {
-			imageContainer = imageContainerRef.current;
-			offsetX = e.clientX - imageContainer.getBoundingClientRect().left;
-			console.log(`e.client: ${e.clientX} ${e.clientY}`)
-			console.log(`${imageContainer.getBoundingClientRect().left} ${imageContainer.getBoundingClientRect().top}`)
-		
-		}
+  const wheelHandler: React.WheelEventHandler<HTMLDivElement> = (e) => {
+    let imageContainer;
+    let offsetX = 0;
 
-		let offsetY = 0;
+    if (imageContainerRef.current) {
+      imageContainer = imageContainerRef.current;
+      offsetX = e.clientX - imageContainer.getBoundingClientRect().left;
+      console.log(`e.client: ${e.clientX} ${e.clientY}`);
+      console.log(
+        `${imageContainer.getBoundingClientRect().left} ${
+          imageContainer.getBoundingClientRect().top
+        }`
+      );
+    }
 
-		if (imageContainerRef.current) {
-			imageContainer = imageContainerRef.current;
-			offsetY = e.clientY - imageContainer.getBoundingClientRect().top;
-		}
+    let offsetY = 0;
 
-		setMouseStartX(offsetX);
-		setMouseStartY(offsetY);
+    if (imageContainerRef.current) {
+      imageContainer = imageContainerRef.current;
+      offsetY = e.clientY - imageContainer.getBoundingClientRect().top;
+    }
 
-		const ratioValue = ratio - 0.001 * e.deltaY;
+    setMouseStartX(offsetX);
+    setMouseStartY(offsetY);
 
-		if (ratioValue >= 0.26 && ratioValue < 0.5) {
-			const slowDownRatioValue = ratioValue * 0.9;
+    const ratioValue = ratio - 0.001 * e.deltaY;
 
-			setRatio(slowDownRatioValue);
-		} else if (ratioValue >= 0.5 && ratioValue < 2.06) {
-			setRatio(ratioValue);
-		}
-	};
+    if (ratioValue >= 0.26 && ratioValue < 0.5) {
+      const slowDownRatioValue = ratioValue * 0.9;
 
-	const Frame = styled.div`
-		position: relative;
-		transition: all 2s ease-out;
-	
-		transform: scale(
-				${({ ratio }: FrameProps) => {
-					console.log(2.2*ratio)
-					return 2.2 * ratio;
-				}}
-			);
+      setRatio(slowDownRatioValue);
+    } else if (ratioValue >= 0.5 && ratioValue < 2.06) {
+      setRatio(ratioValue);
+    }
+  };
 
+  const Frame = styled.div`
+    position: relative;
+    transition: all 2s ease-out;
 
-		transform-origin: ${({ mouseStartX }: FrameProps) => mouseStartX}px
-		${({ mouseStartY }: FrameProps) => mouseStartY}px;
-		
-		
-			
+    transform: scale(
+      ${({ ratio }: FrameProps) => {
+        console.log(2.2 * ratio);
+        return 2.2 * ratio;
+      }}
+    );
 
-	
-	`;
+    transform-origin: ${({ mouseStartX }: FrameProps) => mouseStartX}px
+      ${({ mouseStartY }: FrameProps) => mouseStartY}px;
+  `;
 
-	const Picture = styled.div`
-    width; 100%;
-    `;
+  const Picture = styled.div`
+    width: 100%;
+  `;
 
-	const [popUpShow, setPopUpShow] = useState<boolean>(false);
+  const [popUpShow, setPopUpShow] = useState<boolean>(false);
 
-	useEffect(() => {
-		setPopUpShow(true);
-	}, []);
+  useEffect(() => {
+    setPopUpShow(true);
+  }, []);
 
-	const handlePopupAnimation = () => {
-		setPopUpShow(false);
-	};
+  const handlePopupAnimation = () => {
+    setPopUpShow(false);
+  };
 
-	return (
-		<div className={styles.modal_background} onClick={handleModalOff}>
-			<section className={styles.modal_container}>
-				<div className={styles.grey_background}>
-					{workUrl && (
-						<div className={styles.img_container} ref={imageContainerRef}>
-							<div className={styles.delete_button_cont}>
-								<ModalDeleteButton handleDelete={modalOff} />
-							</div>
-							<Frame
-								ratio={ratio}
-								mouseStartX={mouseStartX}
-								mouseStartY={mouseStartY}
-								ref={frameRef}
-								onWheel={wheelHandler}
-							>
-								<Picture>
-									<img
-										className={styles.work_large_image}
-										src={workUrl}
-										alt=""
-									/>
-								</Picture>
-							</Frame>
-						</div>
-					)}
-				</div>
+  return (
+    <div className={styles.modal_background} onClick={handleModalOff}>
+      <section className={styles.modal_container}>
+        <div className={styles.grey_background}>
+          {workUrl && (
+            <div className={styles.img_container} ref={imageContainerRef}>
+              <div className={styles.delete_button_cont}>
+                <ModalDeleteButton handleDelete={modalOff} />
+              </div>
+              <Frame
+                ratio={ratio}
+                mouseStartX={mouseStartX}
+                mouseStartY={mouseStartY}
+                ref={frameRef}
+                onWheel={wheelHandler}
+              >
+                <Picture>
+                  <img
+                    className={styles.work_large_image}
+                    src={workUrl}
+                    alt=''
+                  />
+                </Picture>
+              </Frame>
+            </div>
+          )}
+        </div>
 
-				<PopupInModal popUpShow={popUpShow} handleOkay={handlePopupAnimation} />
+        <PopupInModal popUpShow={popUpShow} handleOkay={handlePopupAnimation} />
 
-				<KeyDetector sendKeyValue={handleKeyDetector} />
-			</section>
-		</div>
-	);
+        <KeyDetector sendKeyValue={handleKeyDetector} />
+      </section>
+    </div>
+  );
 };
 export default WorkModal;
-
 
 // top: ${({ ratio, mouseStartX }: FrameProps) => {
 // 	return -mouseStartX * ratio + mouseStartX;
@@ -155,8 +151,3 @@ export default WorkModal;
 // left: ${({ ratio, mouseStartY }: FrameProps) => {
 // 	return -mouseStartY * ratio + mouseStartY;
 // }};
-
-
-
-
-
