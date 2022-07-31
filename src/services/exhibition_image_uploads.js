022a509b565b9bce52cf58a7e98127fc9824d339
cho_jae_sin <1005jsc@naver.com>
2022-08-01 00:26:07 +0900

    1

--- src/services/exhibition_image_uploads.js
+++ src/services/exhibition_image_uploads.js
@@ -1,71 +1,38 @@
-
 class ExhibitionImageUpload {
-  static url = "https://api.cloudinary.com/v1_1/koreachief/auto/upload";
+  static url = 'https://api.cloudinary.com/v1_1/koreachief/auto/upload';
 
   uploadSingleImage = async (imageData) => {
     const formData = new FormData();
-    
-    formData.append("file", imageData )
-    formData.append("upload_preset", "lj67huqj")
-    
-    // fetch(this.uploadImageurl, {
-    //   method: "POST",
-    //   body: formData
-    // }).then((res) => {
-    //   return res.json();
-    // })
+
+    formData.append('file', imageData);
+    formData.append('upload_preset', 'lj67huqj');
 
     const res = await fetch(ExhibitionImageUpload.url, {
-      method: "POST",
-      body: formData
-    })
+      method: 'POST',
+      body: formData,
+    });
 
     return await res.json();
+  };
 
-  }  
-
-  uploadMultipleImage = async(multipleImageData) => {
-
-
+  uploadMultipleImage = async (multipleImageData) => {
     const formData = new FormData();
-    const arrayImages = []
+    const arrayImages = [];
     for (let i = 0; i < multipleImageData.length; i++) {
       let imageData = multipleImageData[i];
 
-    formData.append("file", imageData)
-    formData.append("upload_preset", "lj67huqj")
-    const res = await fetch(ExhibitionImageUpload.url, {
-      method: "POST",
-      body: formData
-    })
-  
-    const res2 = await res.json();
-    arrayImages.push(res2)
-  }
-  
-  return arrayImages
+      formData.append('file', imageData);
+      formData.append('upload_preset', 'lj67huqj');
+      const res = await fetch(ExhibitionImageUpload.url, {
+        method: 'POST',
+        body: formData,
+      });
 
+      const res2 = await res.json();
+      arrayImages.push(res2);
+    }
+
+    return arrayImages;
+  };
 }
-
-
-
-
-
-}
-export default ExhibitionImageUpload
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default ExhibitionImageUpload;
